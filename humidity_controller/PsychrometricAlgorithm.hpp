43a6f1dfcdce43744a0d34f2a98ad01b095b8a8c
leonovdmitry <dimonstar67rus@gmail.com>
2018-03-18 21:31:54 +0300

    Update PsychrometricAlgorithm.hpp

--- humidity_controller/PsychrometricAlgorithm.hpp
+++ humidity_controller/PsychrometricAlgorithm.hpp
@@ -28,9 +28,12 @@ class PsychrometricAlgorithm
       const double a = 17.5043;
       const double b = 241.2;
       float pressure = E * exp(abs(a * t / (b + t)));
-      //Hyland and Wexler
+      // Hyland and Wexler
       //t *=274.15;
       //float pressure = log(2.8365*1000/t*t-6.0280*1000/t+1.9542*10-2.7378*t/100+1.6261*t*t/100000+7.0229*t*t*t/10000000000-1.8680*t*t*t*t/10000000000000+2.7150*log(t));
+      // Goff Gratch 
+      //t *=274.15;
+      //float pressure = log10(-7.90298*(373.16/t-1)+ 5.02808*log10(373.16/t)- 1.3816*pow(10,-7)*(pow(10,11.344*(1-t/373.16))-1)+8.1328 *pow(10,-3)*(pow(10,-3.49149*(373.16/t-1))-1)+log10(1013.246));
       return pressure;
     }
 };
